be1a8cb724abcf8236590e266187ca2a433b2384
dewirara <rarassati27@gmail.com>
2024-01-23 22:11:38 +0700

    Perubahan yang telah dilakukan

--- config/proses.php
+++ config/proses.php
@@ -7,6 +7,7 @@ require 'func_setnotify.php';
 require 'func_user_session.php';
 require 'func_uploadFoto.php';
 require 'func_wa.php';
+require 'func_cart.php';
 
 $action = $_GET['action'];
 
@@ -36,12 +37,20 @@ if($action == 'logout') {
         $sql->bindParam(':password', $_POST['password']);
         $sql->execute();
         $result = $sql->fetch();
-        user_session($result);
-        setNotify(0, 'Selamat Datang, ' . $result['nama_awal'] . '!');
-        header('Location: ../index.php');
-    }catch(PDOException $err) {
-        var_dump($err); die;
+
+        if ($result) {
+            user_session($result);
+            setNotify(0, 'Selamat Datang, ' . $result['nama_awal'] . '!');
+            header('Location: ../index.php');
+        } else {
+            setNotify(1, 'Login gagal. Periksa kembali username dan password Anda.');
+            header('Location: ../login.php');
+        }
+    } catch(PDOException $err) {
+        var_dump($err);
+        die;
     }
+
 }else if($action == 'ubah-profile') {
     try{
         $sql = $pdo->prepare('UPDATE users SET nama_awal = :nama_awal, nama_akhir = :nama_akhir, telepon = :telepon WHERE user_id = :user_id');
@@ -92,22 +101,98 @@ if($action == 'logout') {
     }
 }else if($action == 'pesan') {
     try{
-        $sql = $pdo->prepare('INSERT INTO pesan VALUES (NULL, :user_id, :paket, :berat, :subtotal, :total, :nama_awal, :nama_akhir, :alamat, :telepon, :metode_pembayaran, :tanggal_masuk, :tanggal_keluar)');
-        $sql->bindParam(':user_id', $_SESSION['user']['user_id']);
-        $sql->bindParam(':paket', $_POST['paket']);
-        $sql->bindParam(':berat', $_POST['berat']);
-        $sql->bindParam(':subtotal', $_POST['subtotal']);
-        $sql->bindParam(':total', $_POST['total']);
-        $sql->bindParam(':nama_awal', $_POST['nama_awal']);
-        $sql->bindParam(':nama_akhir', $_POST['nama_akhir']);
-        $sql->bindParam(':alamat', $_POST['alamat']);
-        $sql->bindParam(':telepon', $_POST['telepon']);
-        $sql->bindParam(':metode_pembayaran', $_POST['metode_pembayaran']);
-        $sql->bindParam(':tanggal_masuk', $_POST['tanggal_masuk']);
-        $sql->bindParam(':tanggal_keluar', $_POST['tanggal_keluar']);
-        $sql->execute();
-        sendWA($_POST['nama_awal'], $_POST['nama_akhir'], $_POST['paket'], $_POST['berat'], $_POST['subtotal'], $_POST['total'], $_POST['alamat'], $_POST['telepon'], $_POST['metode_pembayaran'], $_POST['tanggal_masuk'], $_POST['tanggal_keluar']);
+        $sql1 = $pdo->prepare('SELECT * FROM cart WHERE user_id = :user_id');
+        $sql1->bindParam(':user_id', $_SESSION['user']['user_id']);
+        $sql1->execute();
+        $totalSemua = 0;
+        if($sql1->rowCount() != 0) {
+            while($row = $sql1->fetch()) {
+                $total = $row['harga'] * $row['jumlah'];
+                $totalSemua += $total;
+                $sql = $pdo->prepare('INSERT INTO pesan VALUES (NULL, :user_id, :paket, :berat, :subtotal, :total, :nama_awal, :nama_akhir, :alamat, :telepon, :metode_pembayaran, :tanggal_masuk, :tanggal_keluar)');
+                $sql->bindParam(':user_id', $_SESSION['user']['user_id']);
+                $sql->bindParam(':paket', $row['paket']);
+                $sql->bindParam(':berat', $row['jumlah']);
+                $sql->bindParam(':subtotal', $row['harga']);
+                $sql->bindParam(':total', $total);
+                $sql->bindParam(':nama_awal', $_POST['nama_awal']);
+                $sql->bindParam(':nama_akhir', $_POST['nama_akhir']);
+                $sql->bindParam(':alamat', $_POST['alamat']);
+                $sql->bindParam(':telepon', $_POST['telepon']);
+                $sql->bindParam(':metode_pembayaran', $_POST['metode_pembayaran']);
+                $sql->bindParam(':tanggal_masuk', $_POST['tanggal_masuk']);
+                $sql->bindParam(':tanggal_keluar', $_POST['tanggal_keluar']);
+                $sql->execute();
+            }
+    
+            $sql2 = $pdo->prepare('DELETE FROM cart WHERE user_id = :user_id');
+            $sql2->bindParam(':user_id', $_SESSION['user']['user_id']);
+            $sql2->execute();
+            
+            sendWA($_POST['nama_awal'], $_POST['nama_akhir'], $totalSemua, $_POST['alamat'], $_POST['telepon'], $_POST['metode_pembayaran'], $_POST['tanggal_masuk'], $_POST['tanggal_keluar']);
+        }else {
+            setNotify(1, 'Pesanan Kosong!, mungkin anda ingin memesan?');
+            header('Location: ../pakaian.php');
+        }
+        
     }catch(PDOException $err) {
         var_dump($err->getMessage()); die;
     }
+}else if($action == 'order') {
+    require 'func_isLogin.php';
+    try{
+        $sql1 = $pdo->prepare('SELECT * FROM cart WHERE user_id = :user_id AND paket = :paket');
+        $sql1->bindParam(':user_id', $_SESSION['user']['user_id']);
+        $sql1->bindParam(':paket', $_GET['paket']);
+        $sql1->execute();
+
+        if($sql1->rowCount() == 0) {
+            $sql2 = $pdo->prepare('INSERT INTO cart VALUES (NULL, :user_id, :paket, 1, :harga)');
+            $sql2->bindParam(':user_id', $_SESSION['user']['user_id']);
+            $sql2->bindParam(':paket', $_GET['paket']);
+            $sql2->bindParam(':harga', $_GET['harga']);
+            $sql2->execute();
+            setNotify(0, 'Dimasukan Ke Cart.');
+        }else {
+            setNotify(1, 'Sudah Ada Didalam Cart!');
+        }
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+    }catch(PDOException $err) {
+        var_dump($err->getMessage()); die;
+    }
+}else if($action == 'set-jumlah') {
+    try{
+        $sql = $pdo->prepare('UPDATE cart SET jumlah = :jumlah, harga = :subtotal WHERE user_id = :user_id AND paket = :paket');
+        $sql->bindParam(':jumlah', $_GET['jml']);
+        $sql->bindParam(':subtotal', $_GET['subtotal']);
+        $sql->bindParam(':user_id', $_SESSION['user']['user_id']);
+        $sql->bindParam(':paket', $_GET['paket']);
+        $sql->execute();
+        die;
+    }catch(PDOException $err) {
+        echo json_encode(array(
+            'error' => array(
+                'code' => $err->getCode(),
+                'message' => $err->getMessage()
+            )
+        ));
+    }
+    
+    echo json_encode(array('ok'));
+}else if($action == 'hapus-cart') {
+    try{
+        $sql = $pdo->prepare('DELETE FROM cart WHERE user_id = :user_id AND paket = :paket');
+        $sql->bindParam(':user_id', $_SESSION['user']['user_id']);
+        $sql->bindParam(':paket', $_GET['paket']);
+        $sql->execute();
+    }catch(PDOException $err) {
+        echo json_encode(array(
+            'error' => array(
+                'code' => $err->getCode(),
+                'message' => $err->getMessage()
+            )
+        ));
+    }
+    
+    echo json_encode(array('ok'));
 }
--- laundry.sql
+++ laundry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 14 Jan 2024 pada 07.38
+-- Waktu pembuatan: 15 Jan 2024 pada 10.32
 -- Versi server: 8.0.30
 -- Versi PHP: 8.1.10
 
@@ -18,100 +18,128 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `laundry`
+-- Database: laundry
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pesan`
+-- Struktur dari tabel cart
 --
 
-CREATE TABLE `pesan` (
-  `pesan_id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `paket` varchar(10) NOT NULL,
-  `berat` int NOT NULL,
-  `subtotal` int NOT NULL,
-  `total` int NOT NULL,
-  `nama_awal` varchar(50) NOT NULL,
-  `nama_akhir` varchar(50) NOT NULL,
-  `alamat` text NOT NULL,
-  `telepon` varchar(15) NOT NULL,
-  `metode_pembayaran` enum('Dana','Ovo','Transfer Bank') NOT NULL,
-  `tanggal_masuk` date NOT NULL,
-  `tanggal_keluar` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data untuk tabel `pesan`
---
-
-INSERT INTO `pesan` (`pesan_id`, `user_id`, `paket`, `berat`, `subtotal`, `total`, `nama_awal`, `nama_akhir`, `alamat`, `telepon`, `metode_pembayaran`, `tanggal_masuk`, `tanggal_keluar`) VALUES
-(19, 15, 'relax', 27, 3500, 94500, 'Sit harum nisi dolor', 'Dolorem error aut co', 'Aut dolorem natus od', '241136602562', 'Transfer Bank', '1991-11-05', '2015-05-26'),
-(20, 15, 'relax', 94, 3500, 329000, 'Nostrum nulla dolore', 'Est ad consequuntur ', 'Deleniti recusandae', '801895203768', 'Transfer Bank', '2024-08-18', '2001-01-04');
+-- Tabel cart
+CREATE TABLE cart (
+  cart_id int NOT NULL AUTO_INCREMENT,
+  -- Kolom-kolom lainnya di sini
+  PRIMARY KEY (cart_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Struktur dari tabel pesan
 --
 
-CREATE TABLE `users` (
-  `user_id` int NOT NULL,
-  `foto` varchar(255) DEFAULT NULL,
-  `username` varchar(12) NOT NULL,
-  `password` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `nama_awal` varchar(50) NOT NULL,
-  `nama_akhir` varchar(50) NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `telepon` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `alamat1` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `alamat2` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE pesan (
+  pesan_id int NOT NULL,
+  user_id int NOT NULL,
+  paket varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  berat int NOT NULL,
+  subtotal int NOT NULL,
+  total int NOT NULL,
+  nama_awal varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  nama_akhir varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  alamat text COLLATE utf8mb4_general_ci NOT NULL,
+  telepon varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  metode_pembayaran enum('Dana','Ovo','Transfer Bank') COLLATE utf8mb4_general_ci NOT NULL,
+  tanggal_masuk date NOT NULL,
+  tanggal_keluar date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data untuk tabel pesan
 --
 
-INSERT INTO `users` (`user_id`, `foto`, `username`, `password`, `nama_awal`, `nama_akhir`, `email`, `telepon`, `alamat1`, `alamat2`) VALUES
-(15, NULL, 'egasuyatna18', NULL, 'Ega', 'Suyatna', 'egasuyatna18@gmail.com', NULL, NULL, NULL);
+INSERT INTO pesan (pesan_id, user_id, paket, berat, subtotal, total, nama_awal, nama_akhir, alamat, telepon, metode_pembayaran, tanggal_masuk, tanggal_keluar) VALUES
+(28, 22, 'sprei', 1, 15000, 15000, 'Perferendis voluptas', 'Quos sed laboris ut ', 'Reprehenderit proide', '605151785135', 'Transfer Bank', '1972-01-12', '1995-07-03'),
+(29, 22, 'bedcover', 1, 30000, 30000, 'Perferendis voluptas', 'Quos sed laboris ut ', 'Reprehenderit proide', '605151785135', 'Transfer Bank', '1972-01-12', '1995-07-03'),
+(30, 22, 'relax', 2, 3500, 7000, 'Esse aut ea ratione ', 'Laboriosam nulla di', 'Qui consectetur ame', '356743642152', 'Transfer Bank', '2004-12-28', '1976-11-03'),
+(31, 22, 'boneka', 2, 10000, 20000, 'Esse aut ea ratione ', 'Laboriosam nulla di', 'Qui consectetur ame', '356743642152', 'Transfer Bank', '2004-12-28', '1976-11-03');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel users
+--
+
+CREATE TABLE users (
+  user_id int NOT NULL,
+  foto varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  username varchar(12) COLLATE utf8mb4_general_ci NOT NULL,
+  password varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  nama_awal varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  nama_akhir varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  email varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  telepon varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  alamat1 text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  alamat2 text COLLATE utf8mb4_general_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel users
+--
+
+INSERT INTO users (user_id, foto, username, password, nama_awal, nama_akhir, email, telepon, alamat1, alamat2) VALUES
+(22, NULL, 'egasuyatna18', NULL, 'Ega', 'Suyatna', 'egasuyatna18@gmail.com', NULL, '111', '111'),
+(23, NULL, '111', '111', '111', '111', '111@111', '111', '111', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `pesan`
+-- Indeks untuk tabel cart
 --
-ALTER TABLE `pesan`
-  ADD PRIMARY KEY (`pesan_id`);
+ALTER TABLE cart
+  MODIFY cart_id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
--- Indeks untuk tabel `users`
+-- Indeks untuk tabel pesan
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE pesan
+  ADD PRIMARY KEY (pesan_id);
+
+--
+-- Indeks untuk tabel users
+--
+ALTER TABLE users
+  ADD PRIMARY KEY (user_id),
+  ADD UNIQUE KEY username (username),
+  ADD UNIQUE KEY email (email);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT untuk tabel `pesan`
+-- AUTO_INCREMENT untuk tabel cart
 --
-ALTER TABLE `pesan`
-  MODIFY `pesan_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+ALTER TABLE cart
+  MODIFY cart_id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT untuk tabel pesan
 --
-ALTER TABLE `users`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+ALTER TABLE pesan
+  MODIFY pesan_id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT untuk tabel users
+--
+ALTER TABLE users
+  MODIFY user_id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
